2597d7a6615b9bc8a758655219ef8aa4dc009a55
Larry Taylor and Max Scher <max@>
2017-06-01 09:15:51 -0700

    add input forms for date and color

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
   var food = $("#food").val();
-
   var animal = $("input:radio[name=animal]:checked").val();
+  var date = $("#date").val();
+  var color = $("#color").val();
 });
